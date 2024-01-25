c4e00afc2fe864a3702e7ea9ab2aa85144f42c9e
tankistttt <levandemetrashvili10@gmail.com>
2024-01-25 20:45:38 +0400

    fixed bugs

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const symbol = document.querySelectorAll('.symbol');
 // starting condition
 const coordinates = ['', '', '', '', '', '', '', '', ''];
 let timer = true;
+let xCounter = 0;
+let oCounter = 0;
+let lastClick;
 let winningCombinations = [
   [0, 1, 2],
   [3, 4, 5],
@@ -22,31 +25,41 @@ coordinates[randomPos] = symbol[randomPos].textContent = 'X';
 for (let i = 0; i < boxes.length; i++) {
   boxes[i].addEventListener('mousedown', function () {
     if (coordinates[i] === '' && timer) {
+      lastClick = 'O';
       coordinates[i] = symbol[i].textContent = 'O';
+    } else {
+      lastClick = '';
     }
   });
   boxes[i].addEventListener('mouseup', function () {
-    timer = false;
-    if (coordinates.includes('')) {
-      do {
-        randomPos = Math.trunc(Math.random() * 9);
-      } while (coordinates[randomPos] !== '');
-    }
-    winningCombinations.forEach((combination, i, arr) => {
-      combination.forEach((pos, i, arr) => {
-        console.log(
-          coordinates.every((coord, index) => {
-            if (index == pos) {
-              coord[index] === 'X';
-            }
-          })
-        );
+    if (lastClick === 'O') {
+      timer = false;
+      if (coordinates.includes('')) {
+        do {
+          randomPos = Math.trunc(Math.random() * 9);
+        } while (coordinates[randomPos] !== '');
+      }
+      winningCombinations.forEach((combination, i, arr) => {
+        xCounter = 0;
+        oCounter = 0;
+        combination.forEach((pos, i, arr) => {
+          //* add count
+          if (coordinates[pos] === 'X') xCounter++;
+          else if (coordinates[pos] === 'O') oCounter++;
+          //* if winner is  add class
+          if (xCounter === 3) {
+            console.log('Winner : X');
+          } else if (oCounter === 3) {
+            boxes[pos].classList.add('blink');
+            console.log('Winner : O');
+          }
+        });
       });
-    });
 
-    setTimeout(() => {
-      coordinates[randomPos] = symbol[randomPos].textContent = 'X';
-      timer = true;
-    }, 500);
+      setTimeout(() => {
+        coordinates[randomPos] = symbol[randomPos].textContent = 'X';
+        timer = true;
+      }, 500);
+    }
   });
 }
